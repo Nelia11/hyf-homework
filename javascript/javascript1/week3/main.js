b37523a50dbc5372ef8a862dad9038c747b58da8
Nelia11 <levit.nelia@icloud.com>
2023-03-13 10:24:41 +0100

    Series duration of my life

--- javascript/javascript1/week3/main.js
+++ javascript/javascript1/week3/main.js
@@ -42,3 +42,51 @@ const travelTime = getTime(travelInformation.destinationDistance,
 
 console.log(travelTime);
 
+//Series duration of my life
+const seriesDurations = [];
+
+seriesDurations.push(
+    {
+    title: "Unorthodox",
+    days: 0,
+    hours: 3,
+    minutes: 36,
+    },
+);
+
+seriesDurations.push(
+    {
+    title: "Wrong Turn",
+    days: 0,
+    hours: 11,
+    minutes: 56,
+    },
+);
+
+seriesDurations.push(
+    {
+    title: "Game of thrones",
+    days: 3,
+    hours: 1,
+    minutes: 0,
+    },
+);
+
+function logOutSeriesText(seriesDurations) {
+    // write code here
+    const minsInYear = 365 * 24 * 60; //convert  1 year in min
+    const minsInLifespan = 80 * minsInYear; // convert lifespan in min
+    let minsSpent = 0;
+
+    for (let i = 0; i < seriesDurations.length; i++) {
+        const currentSeriesDurations = seriesDurations[i];
+        const totalMinsTV = currentSeriesDurations.days * 24 * 60 + currentSeriesDurations.hours * 60 + currentSeriesDurations.minutes;
+        minsSpent += totalMinsTV;
+        const percent = ((minsSpent/minsInLifespan) * 100).toFixed(3);
+        console.log(`${currentSeriesDurations.title} took ${percent}% of my life`);
+    }
+    const totalPercent = ((minsSpent/minsInLifespan) * 100).toFixed(3);
+    console.log(`In total that is ${totalPercent}% of my life`);
+  }
+  
+logOutSeriesText(seriesDurations);
