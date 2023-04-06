6cf865c90207a71f1c6a9c3131b6bc7edc9d8822
Nelia11 <levit.nelia@icloud.com>
2023-04-06 12:25:18 +0200

    add Searching for products

---
+++ javascript/javascript2/week2/hyf-bay/main.js
@@ -0,0 +1,48 @@
+console.log("Script loaded");
+const products = getAvailableProducts();
+//render random products 
+
+function renderProducts(products) {
+    const ulTag = document.querySelector("section ul");
+    products.forEach(product => {
+        const liTag = document.createElement("li");
+        liTag.innerHTML = `
+        <ul>
+            <li>${product.name}</li>
+            <li>price: ${product.price}</li>
+            <li>Rating: ${product.rating}</li>
+        </ul>
+        `;
+    ulTag.appendChild(liTag);
+    })
+}
+renderProducts(products);
+
+//render filtered products by searching the name 
+const inputTag = document.getElementById("searchByName");
+inputTag.addEventListener("input", () => renderFilteredArray(products));
+
+function renderFilteredArray (products) {
+    const filteredArray = products.filter(product => 
+        product.name.toLowerCase().startsWith(inputTag.value.toLowerCase())
+    );
+     
+    const ulTag = document.createElement("ul");
+    filteredArray.forEach(product => {
+        const liTag = document.createElement("li");
+        liTag.innerHTML = `
+            <ul>
+              <li>${product.name}</li>
+              <li>Price: ${product.price}</li>
+              <li>Rating: ${product.rating}</li>
+            </ul>
+        `;
+    ulTag.appendChild(liTag);
+    });
+
+    const result = document.querySelector("section ul");
+    result.innerHTML = "";
+    result.appendChild(ulTag);
+}
+renderFilteredArray(products);
+
