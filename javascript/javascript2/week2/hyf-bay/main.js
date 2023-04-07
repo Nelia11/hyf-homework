d27b5af7d09c1bd28ef8d17444c7794435074930
Nelia11 <levit.nelia@icloud.com>
2023-04-07 15:09:34 +0200

    add sort by price, name, rating; delete; reduce repetition of code

--- javascript/javascript2/week2/hyf-bay/main.js
+++ javascript/javascript2/week2/hyf-bay/main.js
@@ -4,6 +4,7 @@ const products = getAvailableProducts();
 
 function renderProducts(products) {
     const ulTag = document.querySelector("section ul");
+    ulTag.innerHTML = "";
     products.forEach(product => {
         const liTag = document.createElement("li");
         liTag.innerHTML = `
@@ -26,47 +27,35 @@ function renderFilteredArray (products) {
     const filteredArray = products.filter(product => 
         product.name.toLowerCase().startsWith(inputTag.value.toLowerCase())
     );
-     
-    const ulTag = document.createElement("ul");
-    filteredArray.forEach(product => {
-        const liTag = document.createElement("li");
-        liTag.innerHTML = `
-            <ul>
-              <li>${product.name}</li>
-              <li>Price: ${product.price}</li>
-              <li>Rating: ${product.rating}</li>
-            </ul>
-        `;
-    ulTag.appendChild(liTag);
-    });
-
-    const result = document.querySelector("section ul");
-    result.innerHTML = "";
-    result.appendChild(ulTag);
+    renderProducts(filteredArray);
 }
-renderFilteredArray(products);
 
+//render filtered products by maxPrice
 const inputPriceTag = document.getElementById("maxPrice");
 inputPriceTag.addEventListener("input", () => renderMaxPrice(products));
 
 function renderMaxPrice(products) {
     const filteredArray = products.filter(product => product.price <= inputPriceTag.value);
-
-    const ulTag = document.createElement("ul");
-    filteredArray.forEach(product => {
-        const liTag = document.createElement("li");
-        liTag.innerHTML = `
-            <ul>
-              <li>${product.name}</li>
-              <li>Price: ${product.price}</li>
-              <li>Rating: ${product.rating}</li>
-            </ul>
-        `;
-    ulTag.appendChild(liTag);
-    });
-
-    const result = document.querySelector("section ul");
-    result.innerHTML = "";
-    result.appendChild(ulTag);
+    renderProducts(filteredArray);
 }
 
+//sort by price, name, rating
+const selectTag = document.getElementById("dropdown");
+selectTag.addEventListener("change", () => renderSortBy(products));
+
+function renderSortBy(products) {
+    const value = selectTag.value;
+
+    if(value === "price") {
+        products.sort((a, b) => a.price - b.price);
+    } else if(value === "name") {
+        products.sort((a, b) => {
+            const nameA = a.name.replace(/"/g, '').toLowerCase();
+            const nameB = b.name.replace(/"/g, '').toLowerCase();
+            return (nameA > nameB) ? 1 : (nameA < nameB) ? -1 : 0;
+        });
+    } else if(value === "rating") {
+        products.sort((a, b) => b.rating - a.rating);
+    }
+    renderProducts(products);
+}
