268709fe9784c320335cbcadb94510d521c9757c
Nelia11 <levit.nelia@icloud.com>
2023-04-24 09:23:41 +0200

    rename files

---
+++ javascript/javascript2/week1/spritAnimalGenerator.js
@@ -0,0 +1,57 @@
+const buttonTag = document.getElementById("myButton");
+
+function captureName() {
+    const input = document.getElementById("input").value;
+    if (input === "") {
+        document.getElementById("output").innerHTML = "Please enter first name";
+    } else if (/^[a-zA-Z]+$/.test(input)) {
+        document.getElementById("output").innerHTML = input +" - The " + getRandomAnimal(spiritAnimals);
+    } else {
+        document.getElementById("output").innerHTML = "Plese use only letters";
+    }
+}
+
+const selectOption = document.getElementById("dropdown");
+
+selectOption.addEventListener("change", function(){
+    const value = selectOption.value;
+    const inputTag = document.getElementById("input");
+
+    switch(value) {
+        case "click": 
+            buttonTag.addEventListener("click", captureName);
+            inputTag.removeEventListener("mouseover", captureName);
+            inputTag.removeEventListener("input", captureName);
+            break;
+        case "hover": 
+            buttonTag.addEventListener("click", captureName);
+            inputTag.removeEventListener("mouseover", captureName);
+            inputTag.removeEventListener("input", captureName);
+            break;
+        case "isWritten": 
+            inputTag.addEventListener("input", captureName);
+            inputTag.removeEventListener("mouseover", captureName);
+            buttonTag.removeEventListener("click", captureName);
+    }
+})
+
+const spiritAnimals = [
+    "Amazing Unicorn",
+    "Happy Lama",
+    "Mystical Dragon",
+    "Speedy Sloth",
+    "Wild Wolf",
+    "Courageous Pigeon",
+    "Suspicious Racoon",
+    "Aggressive Wasp",
+    "Angry Beaver",
+    "Innocent Snake",
+];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getRandomAnimal (array) {
+    return array[getRandomInt(10)];
+}
