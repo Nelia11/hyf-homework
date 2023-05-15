5e7252186dcbce2e1c09a0466eca5cd600f425ed
Nelia11 <levit.nelia@icloud.com>
2023-05-15 15:22:10 +0200

    add designed queries

---
+++ databases/week1/queries.sql
@@ -0,0 +1,10 @@
+SELECT COUNT(*) FROM task;
+SELECT COUNT(*) FROM task WHERE due_date IS NULL;
+SELECT title FROM task WHERE status_id = 3;
+SELECT title FROM task WHERE status_id != 3;
+SELECT title FROM task ORDER BY created DESC;
+SELECT title FROM task ORDER BY created DESC LIMIT 1;
+SELECT title, due_date FROM task WHERE title LIKE "%database%" OR description LIKE "%database%";
+SELECT task.title, status.name AS status FROM task INNER JOIN status ON task.status_id = status.id;
+SELECT status.name AS status, COUNT(*) AS total FROM status INNER JOIN task ON task.status_id = status.id GROUP BY status.name;
+SELECT status.name AS status, COUNT(*) AS total FROM status INNER JOIN task ON task.status_id = status.id GROUP BY status.name ORDER BY total DESC;
