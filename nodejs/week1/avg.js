ede3b215bcc9eefdb45f9b28632c1b15fffb52c7
Nelia11 <levit.nelia@icloud.com>
2023-06-05 16:02:07 +0200

    add avg.js

---
+++ nodejs/week1/avg.js
@@ -0,0 +1,10 @@
+const newArr = process.argv.slice(2);
+if(newArr.length === 0) {
+    console.log("You did not provide arguments")
+    return;
+} else {
+    const avg = newArr.reduce((acc, val) => Number(acc) + Number(val)) / newArr.length;
+    isNaN(avg)
+        ? console.log("Provided arguments are not numbers")
+        : console.log(avg);
+}
