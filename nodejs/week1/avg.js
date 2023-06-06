c8d76efaf7edf984a4dd9d15ef518ed4dcf0e7de
Nelia11 <levit.nelia@icloud.com>
2023-06-06 15:37:48 +0200

    add some() method

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -3,8 +3,13 @@ if(newArr.length === 0) {
     console.log("You did not provide arguments")
     return;
 } else {
-    const avg = newArr.reduce((acc, val) => Number(acc) + Number(val)) / newArr.length;
-    isNaN(avg)
-        ? console.log("Provided arguments are not numbers")
-        : console.log(avg);
+    const containsNaN = newArr.some(isNaN);
+
+    if(containsNaN) {
+        console.log("Provided arguments are not numbers");
+        return;
+    } else {
+        const avg = newArr.reduce((acc, val) => Number(acc) + Number(val)) / newArr.length;
+        console.log(avg);
+    }
 }
